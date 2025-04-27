8e8e6a8d5755f4a95339406ef45020ad6ef22f0c
lgm1007 <flxl654@gmail.com>
2025-04-27 22:18:06 +0900

    feat: first commit

---
+++ src/test/java/org/gn/gymnote/GymnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gn.gymnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
